cd325298d3710f65989c4e43405ea1ae071b7d55
akashnimare <svnitakash@gmail.com>
2015-06-17 01:04:22 +0530

    chuckit

---
+++ a.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install git
+sudo apt-get install python-pip
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt-get install sublime-text
+sudo add-apt-repository ppa:videolan/stable-daily
+sudo apt-get update
+sudo apt-get install vlc
