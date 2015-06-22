759c3032f11ec89398b9b8ec4ac1f1ff6e364a19
Akash Nimare <svnitakash@gmail.com>
2015-06-22 15:34:52 +0530

    Added nodejs

--- xx.sh
+++ xx.sh
@@ -6,4 +6,7 @@ sudo apt-get update
 sudo apt-get install sublime-text
 sudo add-apt-repository ppa:videolan/stable-daily
 sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+sudo apt-get install -y nodejs
+apt-get install -y build-essential
 sudo apt-get install vlc
