ec5d7ec7c83a8f448681f7e6c7b2702829b267c5
Akash Nimare <svnitakash@gmail.com>
2015-06-26 19:47:09 +0530

    Update xx.sh

--- xx.sh
+++ xx.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-sudo apt-get install git
-sudo apt-get install python-pip
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
-sudo apt-get update
-sudo apt-get install sublime-text
-sudo add-apt-repository ppa:videolan/stable-daily
-sudo apt-get update
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
+sudo add-apt-repository -y ppa:videolan/stable-daily
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
-sudo apt-get install -y nodejs
-apt-get install -y build-essential
-sudo apt-get install vlc
+sudo apt-get update
+sudo apt-get install -y git python-pip build-essential sublime-text nodejs vlc
+
