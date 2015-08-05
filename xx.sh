dc5dafe8b81d9d8c394a46633199c22b5ff54777
Akash Nimare <svnitakash@gmail.com>
2015-08-06 01:34:09 +0530

    Added XDM

--- xx.sh
+++ xx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
 sudo add-apt-repository -y ppa:videolan/stable-daily
+sudo add-apt-repository -y ppa:noobslab/apps  
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 sudo apt-get update
-sudo apt-get install -y git python-pip build-essential sublime-text nodejs vlc
+sudo apt-get install -y git python-pip build-essential sublime-text nodejs vlc xdman
+
 
