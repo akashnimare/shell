c1e8036d7a8970d4d9a6e8c51b37e903db3fdebd
Akash Nimare <svnitakash@gmail.com>
2015-08-13 02:57:36 +0530

    GIMP
    Added gimp 2.8

--- xx.sh
+++ xx.sh
@@ -2,8 +2,9 @@
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
 sudo add-apt-repository -y ppa:videolan/stable-daily
 sudo add-apt-repository -y ppa:noobslab/apps  
+sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 sudo apt-get update
-sudo apt-get install -y git python-pip build-essential sublime-text nodejs vlc xdman
+sudo apt-get install -y git python-pip build-essential sublime-text nodejs vlc xdman gimp
 
 
