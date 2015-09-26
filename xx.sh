97e27717b7426d7a7eec5eca085fc943fb88c064
Akash Nimare <svnitakash@gmail.com>
2015-09-27 04:18:50 +0530

    Virtualenv
    Added virtualenv + virtualwrapper

--- xx.sh
+++ xx.sh
@@ -5,9 +5,9 @@ sudo add-apt-repository -y ppa:noobslab/apps
 sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 sudo apt-get update
-sudo apt-get install -y git python-pip build-essential sublime-text nodejs vlc xdman gimp zsh git-core
+sudo apt-get install -y git python-pip python-dev virtualenv virtualenvwrapper build-essential sublime-text nodejs vlc xdman gimp zsh git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s /usr/bin/zsh
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh - See more at: http://whaaat.com/content/update-shell-zsh-osx-unix#sthash.ORQx1Qb2.dpuf
-
-
+export WORKON_HOME=~/Envs
+source /usr/local/bin/virtualenvwrapper.sh
