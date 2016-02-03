092b7c2c71fb04ad861220da951bf7f3d8b16f1b
Akash Nimare <svnitakash@gmail.com>
2016-02-03 22:56:36 +0530

    St 3

--- shell.sh
+++ shell.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
+#sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
 sudo add-apt-repository -y ppa:videolan/stable-daily
 sudo add-apt-repository -y ppa:noobslab/apps  
 sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 sudo apt-get update
-sudo apt-get install -y git python-pip python-dev virtualenv virtualenvwrapper build-essential sublime-text nodejs vlc xdman gimp zsh git-core
+sudo apt-get install -y unity-tweak-tool gnome-tweak-tool git python-pip python-dev virtualenv virtualenvwrapper build-essential nodejs vlc xdman gimp zsh git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+wget https://download.sublimetext.com/sublime-text_build-3083_amd64.deb
 chsh -s /usr/bin/zsh
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh - See more at: http://whaaat.com/content/update-shell-zsh-osx-unix#sthash.ORQx1Qb2.dpuf
 export WORKON_HOME=~/Envs
 source /usr/local/bin/virtualenvwrapper.sh
+
